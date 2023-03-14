10e9c0d800c278a1bdbf55c38dfcd5c06aafd878
VolodinAlekse <volodin.8989@mail.ru>
2023-03-15 02:38:53 +0300

    final_test_first_commit

---
+++ Final_test/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача:
+// Написать программу, которая из имеющего массива строк формирует массив из строк
+// длина которых меньше либо равна 3 символа.
+// Первоначальный массив можно ввести с клавиатуры,
+// либо задать на старте выполнения алгоритма.
+// При решение не рекомендуется пользоваться коллекциями,
+// лучше обойтись исключительно массивами.
+
+// Примеры:
+
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+Commands();
+string[] array = new string[] { };
+
+string fromUser = ReadInput("Введите команду: ");
+switch (fromUser)
+{
+    case "1":
+        array = new string[] { "Hello", "2", "world", ":-)" };
+        break;
+    case "2":
+        array = new string[] { "1234", "1567", "-2", "computer science" };
+        break;
+    case "3":
+        array = new string[] { "Russia", "Denmark", "Kazan" };
+        break;
+    default:
+        Console.WriteLine($"{fromUser} - Такой команды нет");
+        break;
+}
