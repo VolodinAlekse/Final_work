a048e6e46e35938868cf6eb5d73a5e2379f5b68d
VolodinAlekse <volodin.8989@mail.ru>
2023-03-15 03:26:02 +0300

    final_test_4_commit

--- Final_test/Program.cs
+++ Final_test/Program.cs
@@ -62,3 +62,20 @@ void Commands()
     Console.WriteLine("3 – использовать массив: [“Russia”, “Denmark”, “Kazan”]");
     Console.WriteLine();
 }
+// Функция ввода
+string ReadInput(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}
+
+//  Функция вывода массива в терминал
+void PrintArray(string[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"“{array[i]}”, ");
+    }
+    Console.Write("] ");
+}
